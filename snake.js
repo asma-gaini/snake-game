6fe1c067abfdf13a47d518c9d7a32927cb99cdeb
asma gaini <asma.gaini@yahoo.com>
2023-08-26 19:22:50 +0430

    harkat bar asas time dar jahat bala

--- snake.js
+++ snake.js
@@ -1,11 +1,6 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext('2d');
 
-// rasm boom dar safhe 
-ctx.fillStyle = 'white';
-ctx.strokeStyle = 'black';
-ctx.fillRect(0 , 0 , gameCanvas.width , gameCanvas.height);
-ctx.strokeRect(0 , 0 , gameCanvas.width , gameCanvas.height);
 
 let snake = [
    {x : 150 , y : 150},
@@ -15,17 +10,27 @@ let snake = [
    {x : 110 , y : 150}
 ]
 
-// rasm kardan mokhtasat snake dar boom
-snake.forEach(snakePart => {
-    ctx.fillStyle = "green";
-    ctx.strokeStyle = "black";
-
-    ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
-    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
-});
-
 let foodX ;
 let foodY;
+ 
+function main() {
+    setTimeout(() => {
+        clearCanvas();
+        drowFood();
+        advanceSnake();
+        drowSnake();
+
+        main();
+    }, 300)
+}
+
+// rasm boom dar safhe va har bar pak kardan an
+clearCanvas = () => {
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.fillRect(0 , 0 , gameCanvas.width , gameCanvas.height);
+    ctx.strokeRect(0 , 0 , gameCanvas.width , gameCanvas.height);
+}
 
 // eejade mokhtasat baraye food 
 let randomNumber = (max , min) => Math.round((Math.random() * (max - min) + min ) /10) *10;
@@ -41,9 +46,31 @@ let creatFood = () => {
     })
 }
 
+// harekat snak dar jahat bala 
+let advanceSnake = () => {
+    const head = {x : snake[0].x + 0 , y: snake[0].y - 10};
+    snake.unshift(head);
+    snake.pop();
+} 
+
+// rasm kardan mokhtasat snake dar boom
+let drowSnake = () => snake.forEach(drowSnakePart);
+let drowSnakePart = snakePart => {
+    ctx.fillStyle = "green";
+    ctx.strokeStyle = "black";
+
+    ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
+    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
+}
+
 //  rasme food dar boom
+let drowFood = () => {
+    ctx.fillStyle = "red";
+    ctx.strokeStyle = "darkred";
+    ctx.fillRect(foodX , foodY , 10 , 10);
+    ctx.strokeRect(foodX , foodY , 10 , 10);
+}
 creatFood();
-ctx.fillStyle = "red";
-ctx.strokeStyle = "darkred";
-ctx.fillRect(foodX , foodY , 10 , 10);
-ctx.strokeRect(foodX , foodY , 10 , 10);
+
+
+main();
