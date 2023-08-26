d81019bcc9ca94b8ea8e2a4fb60dced9f0ef907a
asma gaini <asma.gaini@yahoo.com>
2023-08-26 17:00:50 +0430

    crat food with random in boom

--- snake.js
+++ snake.js
@@ -1,5 +1,7 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext('2d');
+
+// rasm boom dar safhe 
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'black';
 ctx.fillRect(0 , 0 , gameCanvas.width , gameCanvas.height);
@@ -13,6 +15,7 @@ let snake = [
    {x : 110 , y : 150}
 ]
 
+// rasm kardan mokhtasat snake dar boom
 snake.forEach(snakePart => {
     ctx.fillStyle = "green";
     ctx.strokeStyle = "black";
@@ -20,3 +23,27 @@ snake.forEach(snakePart => {
     ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
     ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
 });
+
+let foodX ;
+let foodY;
+
+// eejade mokhtasat baraye food 
+let randomNumber = (max , min) => Math.round((Math.random() * (max - min) + min ) /10) *10;
+
+// food dar noghte goshe ha ya ro khode snake nayofte
+let creatFood = () => {
+    foodX = randomNumber(0 , gameCanvas.width-10);
+    foodY = randomNumber(0 , gameCanvas.height-10);
+    snake.forEach((snakePart) => {
+        if (snake.x == foodX && snakePart.y ==foodY) {
+            creatFood();
+        }
+    })
+}
+
+//  rasme food dar boom
+creatFood();
+ctx.fillStyle = "red";
+ctx.strokeStyle = "darkred";
+ctx.fillRect(foodX , foodY , 10 , 10);
+ctx.strokeRect(foodX , foodY , 10 , 10);
