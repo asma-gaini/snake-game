1d99f7cad8a1ba2bf6ef81e998b90dae8501259e
asma gaini <asma.gaini@yahoo.com>
2023-08-28 15:05:04 +0430

    complete snake game and Sort program codes

--- snake.js
+++ snake.js
@@ -1,8 +1,18 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext('2d');
 
+let snake = [
+    {x : 150 , y : 150},
+    {x : 140 , y : 150},
+    {x : 130 , y : 150},
+    {x : 120 , y : 150},
+    {x : 110 , y : 150}
+]
+
+// Food location
 let foodX ;
 let foodY;
+//  snake path direction 
 let dx = 10;
 let dy = 0;
 score = 0;
@@ -16,48 +26,12 @@ let changingDirection = false;
 
 // harekat snak dar jahat haye mokhtalef
 document.addEventListener('keydown' , changeDirection);
-function changeDirection(event){
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39;
-    const Up_KEY = 38;
-    const DOWN_KEY = 40;
-
-    if( changingDirection )
-        return;
-
-    changingDirection = true;
-
-    const keyPressed = event.keyCode;
-    if (keyPressed == LEFT_KEY && dx != 10) {
-        dx = -10 ;
-        dy = 0;
-    }
-    if(keyPressed == RIGHT_KEY && dx != -10) {
-        dx = 10 ;
-        dy = 0;
-    }
-    if (keyPressed == Up_KEY && dy != 10) {
-        dx = 0 ;
-        dy = -10;  
-    }
-    if (keyPressed == DOWN_KEY && dy != -10) {
-        dx = 0 ;
-        dy = 10;
-    }
-}
-
-let snake = [
-   {x : 150 , y : 150},
-   {x : 140 , y : 150},
-   {x : 130 , y : 150},
-   {x : 120 , y : 150},
-   {x : 110 , y : 150}
-]
 
 main();
+creatFood();
+
 
 function main() {
-
     if(didGameEnd())
         return;
 
@@ -88,7 +62,7 @@ function didGameEnd(){
 }
 
 // rasm boom dar safhe va har bar pak kardan an
-clearCanvas = () => {
+function clearCanvas() {
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'black';
     ctx.fillRect(0 , 0 , gameCanvas.width , gameCanvas.height);
@@ -96,10 +70,12 @@ clearCanvas = () => {
 }
 
 // eejade mokhtasat baraye food 
-let randomNumber = (max , min) => Math.round((Math.random() * (max - min) + min ) /10) *10;
+function randomNumber(max , min) {
+    return Math.round((Math.random() * (max - min) + min ) /10) *10;
+} 
 
 // food dar noghte goshe ha ya ro khode snake nayofte
-let creatFood = () => {
+function creatFood() {
     foodX = randomNumber(0 , gameCanvas.width-10);
     foodY = randomNumber(0 , gameCanvas.height-10);
     snake.forEach((snakePart) => {
@@ -110,7 +86,7 @@ let creatFood = () => {
 }
 
 // ejra va harekat snake dar jahat ha ba b vojod avordan yek head va sar jadid baraye snak va delete dom
-let advanceSnake = () => {
+function advanceSnake() {
     const head = {x : snake[0].x + dx , y : snake[0].y + dy};
     snake.unshift(head);
 
@@ -128,8 +104,11 @@ let advanceSnake = () => {
 
 
 // rasm kardan mokhtasat snake dar boom
-let drowSnake = () => snake.forEach(drowSnakePart);
-let drowSnakePart = snakePart => {
+function drowSnake() {
+    return snake.forEach(drowSnakePart);
+}
+
+function drowSnakePart(snakePart){
     ctx.fillStyle = "green";
     ctx.strokeStyle = "black";
 
@@ -138,12 +117,40 @@ let drowSnakePart = snakePart => {
 }
 
 //  rasme food dar boom
-let drowFood = () => {
+function drowFood() {
     ctx.fillStyle = "red";
     ctx.strokeStyle = "darkred";
     ctx.fillRect(foodX , foodY , 10 , 10);
     ctx.strokeRect(foodX , foodY , 10 , 10);
 }
-creatFood();
 
+function changeDirection(event){
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const Up_KEY = 38;
+    const DOWN_KEY = 40;
+
+    if( changingDirection )
+        return;
+
+    changingDirection = true;
+
+    const keyPressed = event.keyCode;
+    if (keyPressed == LEFT_KEY && dx != 10) {
+        dx = -10 ;
+        dy = 0;
+    }
+    if(keyPressed == RIGHT_KEY && dx != -10) {
+        dx = 10 ;
+        dy = 0;
+    }
+    if (keyPressed == Up_KEY && dy != 10) {
+        dx = 0 ;
+        dy = -10;  
+    }
+    if (keyPressed == DOWN_KEY && dy != -10) {
+        dx = 0 ;
+        dy = 10;
+    }
+}
 
